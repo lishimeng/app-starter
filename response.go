a5189eb599e51430490d018a129f25b67fd50e4e
alex li <alex.dou@qq.com>
2022-05-01 00:25:43 +0800

    transaction

--- response.go
+++ response.go
@@ -1,17 +1,30 @@
 package app
 
+import "math"
+
 type Response struct {
-	Code interface{} `json:"code,omitempty"`
+	Code    interface{} `json:"code,omitempty"`
 	Success interface{} `json:"success,omitempty"`
-	Message string `json:"message,omitempty"`
+	Message string      `json:"message,omitempty"`
 }
 
 type Pager struct {
-	TotalPage int `json:"totalPage"`// 总页数
-	PageSize int `json:"pageSize"`// 页面大小
-	PageNum int `json:"pageNum"` // 页号
-	More int `json:"more"` // 是否有下一页
-	Data []interface{} `json:"items,omitempty"`
+	TotalPage int           `json:"totalPage"` // 总页数
+	PageSize  int           `json:"pageSize"`  // 页面大小
+	PageNum   int           `json:"pageNum"`   // 页号
+	More      int           `json:"more"`      // 是否有下一页
+	Data      []interface{} `json:"items,omitempty"`
+}
+
+func (p *Pager) Offset() int {
+	return (p.PageNum - 1) * p.PageSize
+}
+
+func (p *Pager) Total(count int64) int {
+	t := math.Ceil(float64(count) / float64(p.PageSize))
+	totalPage := int(t)
+	p.TotalPage = totalPage
+	return totalPage
 }
 
 type PagerResponse struct {
