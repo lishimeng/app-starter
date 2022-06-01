1ff281d514f4a88eaa0c3656afb1160a77b4fc67
alex li <alex.dou@qq.com>
2022-06-01 16:17:23 +0800

    c

--- application/repo/init.go
+++ application/repo/init.go
@@ -4,8 +4,12 @@ import "github.com/lishimeng/go-orm"
 
 func Database(config persistence.BaseConfig, models ...interface{}) (err error) {
 
-	config.RegisterModel(models...)
-	err = persistence.InitOrm(config)
+	// 此处配置默认数据库 alias=default
+	//config.RegisterModel(models...)
+	//err = persistence.InitOrm(config)
+
+	persistence.RegisterModels(models...)
+	err = persistence.RegisterDatabase(config)
 	if err != nil {
 		return
 	}
