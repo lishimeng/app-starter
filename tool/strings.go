37611efad030f226b17594d13b409202e2b461df
alex li <alex.dou@qq.com>
2022-08-12 15:28:34 +0800

    tool#string

--- tool/strings.go
+++ tool/strings.go
@@ -1,3 +1,14 @@
 package tool
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 // 字符串工具包
+
+func GetRandomString(n int) string {
+	randBytes := make([]byte, n/2)
+	rand.Read(randBytes)
+	return fmt.Sprintf("%x", randBytes)
+}
