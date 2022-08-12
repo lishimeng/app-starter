e9730d375407ea7d56296c2f9eb317caba279305
alex li <alex.dou@qq.com>
2022-08-12 15:58:35 +0800

    tool#db_model

---
+++ tool/models.go
@@ -0,0 +1,22 @@
+package tool
+
+import "time"
+
+type Pk struct {
+	// ID
+	Id int `orm:"pk;auto;column(id)"`
+}
+
+type TableChangeInfo struct {
+	// 状态
+	Status int `orm:"column(status)"`
+	// 创建时间
+	CreateTime time.Time `orm:"auto_now_add;type(datetime);column(ctime)"`
+	// 修改时间
+	UpdateTime time.Time `orm:"auto_now;type(datetime);column(mtime)"`
+}
+
+type TableInfo struct {
+	// 创建时间
+	CreateTime time.Time `orm:"auto_now_add;type(datetime);column(ctime)"`
+}
