db28628c4c71bcdc2984c08b75a50894387f5dce
alex.dou <alex.dou@qq.com>
2023-05-26 17:09:53 +0800

    date format

--- tool/time.go
+++ tool/time.go
@@ -3,7 +3,7 @@ package tool
 import "time"
 
 const (
-	DefaultTimeFormatter = "2006-01-02:15:04:05"
+	DefaultTimeFormatter = "2006-01-02 15:04:05"
 	DateFormatter        = "20060102"
 	DatePlusFormatter    = "2006-01-02"
 	TimeFormatter        = "150405"
