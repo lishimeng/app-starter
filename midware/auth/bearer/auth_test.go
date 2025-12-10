835081e9599385af2777b33d6a9665c7a56f7f21
alex.dou <alex.dou@qq.com>
2025-12-11 01:13:51 +0800

    支持cookie_auth

---
+++ midware/auth/bearer/auth_test.go
@@ -0,0 +1,8 @@
+package bearer
+
+import "testing"
+
+func TestName(t *testing.T) {
+	ForceCookie = true
+	HttpsCookie = true
+}
--- midware/auth/bearer/authorization.go
+++ midware/auth/bearer/authorization.go
@@ -1,9 +1,12 @@
 package bearer
 
 import (
+	"net/http"
+	"strings"
+
+	"github.com/kataras/iris/v12"
 	"github.com/lishimeng/app-starter/server"
 	"github.com/lishimeng/go-log"
-	"strings"
 )
 
 const (
@@ -11,8 +14,59 @@ const (
 	Realm      = "Bearer "
 )
 
+var ForceCookie = false
+var HttpsCookie = false
+var CookieName = "token"
+
+func getCookieAuth(ctx server.Context) string {
+	c := ctx.C.GetCookie(CookieName)
+	return c
+}
+
+func RemoveCookieAuth(ctx server.Context) {
+	ctx.C.SetCookie(&iris.Cookie{
+		Name:     CookieName,
+		Value:    "",
+		Path:     "/",
+		MaxAge:   -1,
+		HttpOnly: true,
+		Secure:   HttpsCookie,
+	})
+}
+
+func SaveCookieAuth(ctx server.Context, token string, ttlHour int, domain string) {
+	sameSiteMode := http.SameSiteDefaultMode
+	if HttpsCookie {
+		sameSiteMode = http.SameSiteStrictMode
+	}
+	if len(domain) > 0 && !strings.HasPrefix(domain, ".") {
+		domain = "." + domain
+	}
+	cookie := iris.Cookie{
+		Name:     CookieName,     // Cookie 名称
+		Value:    token,          // 业务值（如 JWT）
+		Path:     "/",            // 生效路径
+		MaxAge:   3600 * ttlHour, // 有效期（秒），24小时
+		HttpOnly: true,           // 核心：开启 HttpOnly
+		Secure:   HttpsCookie,    // 本地测试设为 false，生产 HTTPS 必须设为 true
+		SameSite: sameSiteMode,   // 防止 CSRF
+		Domain:   domain,
+	}
+	ctx.C.SetCookie(&cookie)
+}
+
 func GetAuth(ctx server.Context) (auth string, ok bool) {
 
+	auth = getCookieAuth(ctx)
+	if len(auth) > 0 {
+		ok = true
+		return
+	}
+	if ForceCookie {
+		auth = ""
+		ok = false
+		return
+	}
 	header := ctx.C.GetHeader(AuthHeader)
 	if len(header) <= 0 {
 		log.Debug("no auth")
